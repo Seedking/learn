43f2489fa717eee43594f15178fcec3a110c235d
Seedking <875828815@qq.com>
2023-08-24 03:36:50 +0800

    addTwoNumbers completed 指针啊指针

--- 2_addTwoNumbers/addTwoNumbers.c
+++ 2_addTwoNumbers/addTwoNumbers.c
@@ -3,6 +3,7 @@
 //
 #include "malloc.h"
 #include "stdio.h"
+#include "stdbool.h"
 
 struct ListNode {
     int val;
@@ -33,13 +34,35 @@ struct ListNode *gen(int *list, int max) {
     return ret;
 }
 
+//15ms 7.5mb
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
     struct ListNode * ret = malloc(sizeof (struct ListNode));
     struct ListNode * old = ret;
     int carry = 0;
-    int now;
-    while(1) {
-
+    int sum;
+    bool running = true;
+    while(l1 || l2) {
+        sum = carry;
+        sum = (l1) ? sum + l1->val : sum;
+        sum = (l2) ? sum + l2->val : sum;
+        l1 = (l1) ?l1->next : NULL;
+        l2 = (l2) ?l2->next : NULL;
+        carry = sum / 10;
+        old->val = sum % 10;
+        running = l1 || l2;
+        if (running) {
+            old->next = malloc(sizeof(struct ListNode));
+            old = old->next;
+            old->next = NULL;
+        } else {
+            old->next = NULL;
+        }
+    }
+    if (carry > 0) {
+        old->next = malloc(sizeof (struct ListNode));
+        old->next->next = NULL;
+        old->next->val = carry;
+    }
     return ret;
 }
 
@@ -50,18 +73,9 @@ int main () {
     const int array_B[] = {9,9,9,9};
     int * p_A = (int *) &array_A;
     int * p_B = (int *) &array_B;
-    struct ListNode * list_A = gen(p_A, sizeof (array_A)/4);
-    struct ListNode * list_B = gen(p_B, sizeof (array_B)/4);
+    struct ListNode * list_A = gen(p_A, 7);
+    struct ListNode * list_B = gen(p_B, 4);
     struct ListNode * ret = addTwoNumbers(list_A, list_B);
-
-    while (1) {
-        printf("%d\n",ret);
-        if (ret->next){
-            ret = ret->next;
-        } else {
-            break;
-        }
-    }
 
     return 0;
 }
