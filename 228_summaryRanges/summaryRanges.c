2026bc5191a8129f5b2ccfe3669eb527ceec702b
Seedking <875828815@qq.com>
2023-08-27 19:02:51 +0800

    summaryRanges completed

--- 228_summaryRanges/summaryRanges.c
+++ 228_summaryRanges/summaryRanges.c
@@ -4,38 +4,39 @@
 #include "malloc.h"
 #include "stdio.h"
 #include "string.h"
+#include "stdbool.h"
+#include "math.h"
 
-char ** summaryRanges(int* nums, int numsSize, int* returnSize){
-    char ** ret = malloc(sizeof (char *)*numsSize);
-    int * cache = malloc(sizeof (int )*numsSize);
-    int ret_count = 0;
-    int cache_count = 0;
-
+char **summaryRanges(int *nums, int numsSize, int *returnSize) {
+    char **ret = malloc(sizeof(char *) * numsSize);
+    *returnSize = 0;
+    bool s = false;
+    bool c;
+    int low, high;
     for (int i = 0; i < numsSize; ++i) {
-        if (i < numsSize - 1 && (nums[i+1] - nums[i]) > 1){
-            cache[cache_count] = nums[i];
-            cache_count++;
-            ++i;
-            cache[cache_count] = nums[i];
-            cache_count++;
-        } else {
-            if (cache_count- 1 > 1) {
-                ret[ret_count] = malloc(sizeof (char)*4);
-                sprintf(ret[ret_count],"%d%s%d", cache[0], "->" ,cache[cache_count]);
-                ret_count++;
-            } else {
-                ret[ret_count] = malloc(sizeof (char));
-                sprintf(ret[ret_count], "%d", cache[cache_count]);
-                ret_count++;
-            }
+        low = i;
+        i++;
+        while (i < numsSize && nums[i] == nums[i-1] +1) {
+            i++;
+            s=true;
         }
+        if (~s) {
+            --i;
+        }
+        high = i;
+        ret[*returnSize] = malloc(sizeof(char) * 25);
+        if (low == high) {
+            sprintf(ret[*returnSize], "%d", nums[low]);
+        } else {
+            sprintf(ret[*returnSize], "%d%s%d", nums[low], "->", nums[high]);
+        }
+        *returnSize += 1;
     }
-    returnSize[0] = ret_count;
     return ret;
 }
 
 int main() {
-    int array[] = {0, 1, 2, 4, 5, 7};
+    int array[] = {0,1,2,4,5,7};
     int *pa = array;
     int rs = 0;
     int *prs = &rs;
