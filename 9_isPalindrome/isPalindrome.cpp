9a7ef7a9e92764e8930e70164f438bf64ffde6e4
Seedking <875828815@qq.com>
2023-08-30 01:34:34 +0800

    modify isPalindrome.cpp

--- 9_isPalindrome/isPalindrome.cpp
+++ 9_isPalindrome/isPalindrome.cpp
@@ -2,15 +2,19 @@
 // Created by Seedking on 2023/8/29.
 //
 #include "iostream"
+#include "vector"
+
+using namespace std;
+//4ms 5.9mb
 class Solution {
 public:
     bool isPalindrome(int x) {
-        std::string s = std::to_string(x);
-        if (s[0] == '-') {
+        if (x<0) {
             return false;
         } else {
-            for (int i = 0; i < s.length()/2; ++i) {
-                if (s[i] != s[s.length()-1-i]) {
+            std::string s = std::to_string(x);
+            for (int i = 0; i < s.length() / 2; ++i) {
+                if (s[i] != s[s.length() - 1 - i]) {
                     return false;
                 }
             }
