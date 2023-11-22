bb504bae043adf9956e3e7913d9b2cf42c61b759
Seedking <875828815@qq.com>
2023-11-22 19:14:56 +0800

    add 263

---
+++ learn_leetcode/csharp/_263_IsUgly/Solution.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Solution
+{
+    static int[] intArray = new[] { 2, 3, 5 };
+    public bool IsUgly(int n)
+    {
+        if (n <= 0) return false;
+        foreach (var i in intArray)
+        {
+            while (n % i == 0)
+            {
+                n/= i;
+            }
+        }
+
+        return n == 1;
+    }
+}
