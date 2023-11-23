6f2a89e874ef5bb3898abe80b1b0d426a1bb7743
Seedking <875828815@qq.com>
2023-11-23 12:34:44 +0800

    add LCR 135

---
+++ learn_leetcode/csharp/_LCR_135_CountNumbers/Solution.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+public class Solution {
+    public int[] CountNumbers(int cnt)
+    {
+        int num = 1;
+        int max = 1;
+        for (int i = 0; i < cnt; i++)
+        {
+            max *= 10;
+        }
+
+        int[] ret = new int[max-1];
+
+        for (int i = 0; i < max-1; i++)
+        {
+            ret[i] = num;
+            num += 1;
+        }
+
+        return ret;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(new Solution().CountNumbers(1).ToString());
+    }
+}
