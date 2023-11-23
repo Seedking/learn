ec2bc390c32f52e587948cdb8a3a59fdef60d684
Seedking <875828815@qq.com>
2023-11-23 18:40:14 +0800

    add LCR 179

---
+++ learn_leetcode/csharp/_LCR_179_TwoSum/Solution.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public int[] TwoSum(int[] price, int target)
+    {
+        int[] ret = {};
+        int low = 0, high = price.Length - 1;
+        while (low < high)
+        {
+            int int_temp = price[low] + price[high];
+            if (int_temp == target)
+            {
+                ret = new[] { price[low] , price[high] };
+                break;
+            }
+            else
+            {
+                if (int_temp > target)
+                {
+                    high--;
+                }
+                else
+                {
+                    low++;
+                }
+            }
+        }
+        return ret;
+    }
+}
