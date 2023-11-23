47c305ae52f9c14cd3b143ae0f928a3226e58c2a
Seedking <875828815@qq.com>
2023-11-23 13:40:08 +0800

    add LCR 159

---
+++ learn_leetcode/csharp/_LCR_159_InventoryManagement/_LCR_159_InventoryManagement/Solution.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public int[] InventoryManagement(int[] stock, int cnt)
+    {
+        Array.Sort(stock);
+        int[] ret = new int[cnt];
+        for (int i = 0; i < cnt; i++)
+        {
+            ret[i] = stock[i];
+        }
+
+        return ret;
+    }
+    
+}
