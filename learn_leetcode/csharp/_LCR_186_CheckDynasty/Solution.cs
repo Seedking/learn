b6aa6ca8442b41c96d89fc286050331f17de6b50
Seedking <875828815@qq.com>
2023-11-24 10:28:31 +0800

    add LCR 186

---
+++ learn_leetcode/csharp/_LCR_186_CheckDynasty/Solution.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public bool CheckDynasty(int[] places) {
+        Array.Sort(places);
+        int len = places.Length;
+        int num = 0, zero = 0;
+        for (int i = 0; i < len; i++)
+        {
+            if (places[i] == 0)
+            {
+                zero += 1;
+            }
+            else
+            {
+                num = i;
+                break;
+            }
+        }
+
+        for (int i = num + 1; i < len; i++)
+        {
+            if (places[i] == places[i-1])
+            {
+                return false;
+            }
+
+            if (places[i] == places[i-1] + 1)
+            {
+                continue;
+            }
+            else
+            {
+                int int_temp = places[i] - places[i - 1] -1;
+                if (zero > 0 && zero - int_temp >= 0)
+                {
+                    zero -= int_temp;
+                    continue;
+                }
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        int[] places = { 0,0,8,5,4 };
+        Console.WriteLine(new Solution().CheckDynasty(places));
+    }
+}
