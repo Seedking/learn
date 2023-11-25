f38424820b5e4219fe256cd97829317828b40386
Seedking <875828815@qq.com>
2023-11-25 21:26:42 +0800

    add LCR 088

---
+++ learn_leetcode/csharp/_LCR_088_MinCostClimbingStairs/_LCR_088_MinCostClimbingStairs/Solution.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int len = cost.Length;
+        int low = 0, high = 0;
+        for (int i = 2; i <= len; i++)
+        {
+            int next = Math.Min(low + cost[i - 1], high + cost[i - 2]);
+            high = low;
+            low = next;
+        }
+
+        return low;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(new Solution().MinCostClimbingStairs(new []{10,15,20}));
+    }
+}
