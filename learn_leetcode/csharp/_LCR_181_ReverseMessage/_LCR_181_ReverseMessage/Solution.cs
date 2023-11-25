d5cc7e512323f0a3c42c50d0be26db701f47d944
Seedking <875828815@qq.com>
2023-11-25 14:21:31 +0800

    add LCR 181

---
+++ learn_leetcode/csharp/_LCR_181_ReverseMessage/_LCR_181_ReverseMessage/Solution.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public string ReverseMessage(string message)
+    {
+        if (message == "")
+        {
+            return "";
+        }
+        
+        return string.Join(" ", message.Split().Where(x => x != "").Reverse().ToArray());
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(new Solution().ReverseMessage("        114 514 1919 810 "));
+    }
+}
