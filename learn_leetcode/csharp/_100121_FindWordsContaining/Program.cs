38146447d0a9d2d1ce8aa2b55f6d04acc0b66999
Seedking <875828815@qq.com>
2023-11-25 23:06:25 +0800

    add 100121

---
+++ learn_leetcode/csharp/_100121_FindWordsContaining/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Solution {
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        int len = words.Length;
+        IList<int> ret = new List<int>();
+        for (int i = 0; i < len; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                ret.Add(i);
+            }
+        }
+
+        return ret;
+    }
+}
