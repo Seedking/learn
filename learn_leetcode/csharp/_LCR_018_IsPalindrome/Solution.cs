d19b4ec86bbe88af31af670fbd89cbaeb87a225b
Seedking <875828815@qq.com>
2023-11-26 12:53:27 +0800

    add LCR 018

---
+++ learn_leetcode/csharp/_LCR_018_IsPalindrome/Solution.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public bool IsPalindrome(string s)
+    {
+        var strTemp = s.ToLower().Replace(" ", "").Where(c => char.IsLetterOrDigit(c)).ToArray();
+        int low = 0, high = strTemp.Length - 1;
+        while (low < high)
+        {
+            if (strTemp[low] == strTemp[high])
+            {
+                low++;
+                high--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(new Solution().IsPalindrome("0P"));
+    }
+}
