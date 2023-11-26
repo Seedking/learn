10d2163c461e9b572ff3804e639e3ff082197480
Seedking <875828815@qq.com>
2023-11-26 13:13:41 +0800

    add LCR 068

---
+++ learn_leetcode/csharp/_LCR_068_SearchInsert/Solution.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target)
+    {
+        int low = 0, high = nums.Length - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target)
+            {
+                low++;
+            }
+            else if (nums[mid] > target)
+            {
+                high--;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return low;
+    }
+}
