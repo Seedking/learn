cd78d242660d0ea33a7b55d8a65ab82420e563b2
Seedking <875828815@qq.com>
2023-11-26 14:50:32 +0800

    add LCR 019

---
+++ learn_leetcode/csharp/_LCR_019_ValidPalindrome/Solution.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public bool checkBranch(ref string s, int low, int high)
+    {
+        while (low <= high)
+        {
+            if (s[low] == s[high])
+            {
+                low++;
+                high--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public bool ValidPalindrome(string s)
+    {
+        int low = 0, high = s.Length - 1;
+        while (low <= high)
+        {
+            if (s[low] == s[high])
+            {
+                low++;
+                high--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return checkBranch(ref s, low + 1, high) || checkBranch(ref s, low, high - 1);
+    }
+}
